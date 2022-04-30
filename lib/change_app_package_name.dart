cf187ca1931a9e1c8dfdefb39944fe43d8d7efa5
Vaibhavsaharan <vaibhavsaharans@gmail.com>
2022-04-30 15:14:53 +0530

    Update change_app_package_name.dart
    added label in arguments

--- lib/change_app_package_name.dart
+++ lib/change_app_package_name.dart
@@ -6,10 +6,10 @@ class ChangeAppPackageName {
   static void start(List<String> arguments) {
     if (arguments.isEmpty) {
       print('New package name is missing in paraments. please try again.');
-    } else if (arguments.length > 1) {
-      print('Wrong arguments, this package accepts only new package name');
+    } else if (arguments.length > 2) {
+      print('Wrong arguments, this package accepts new package name and new label');
     } else {
-      AndroidRenameSteps(arguments[0]).process();
+      AndroidRenameSteps(arguments[0], arguments[1]).process();
     }
   }
 }
